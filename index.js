36793c841c1b41ae24f9fa52536401ca2b7e7153
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-23 12:15:24 -0400

    updated style of index js file

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var newman = require('newman');
 
-
-newman.run({
+newman.run(
+  {
     collection: 'https://www.getpostman.com/collections/07f9c25f7df2188715bb',
-    reporters: 'cli'
-}, function (err) {
-	if (err) { throw err; }
-    console.log('collection run complete!');
-});
+    reporters: 'cli',
+    color: 'on'
+  }, 
+  function (err) {
+	  if (err) { 
+      throw err; 
+    }
+    console.log('collection run complete!');  
+  }
+);
