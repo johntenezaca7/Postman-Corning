ba0a16ea7292ea85fcc90eaf54997a4380f0a40c
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-24 10:02:47 -0400

    Added html reporter for newman, updated JS file interface

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var newman = require('newman');
+var NAME = 'CORNING ENDPOINT'
 
-newman.run(
-  {
+newman.run({
     collection: 'https://www.getpostman.com/collections/07f9c25f7df2188715bb',
-    reporters: 'cli',
+    reporters: ['cli', 'htmlextra'],
     color: 'on'
-  }, 
-  function (err) {
-	  if (err) { 
-      throw err; 
+  })
+  .on('start', 
+    function (err, args) { 
+      console.log(`Running a collection for ${ NAME }.`);
     }
-    console.log('collection run complete!');  
-  }
-);
+  )
+  .on('done', 
+    function (err, summary) {
+      if (err || summary.error) {
+        console.error(`Collection run encountered an ERROR: ${ err || summary.error }.`);
+      }
+      else {
+        console.log(`Collection for ${ NAME } run completed.`);
+      }
+    }
+  );
