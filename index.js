663fd34b6bd541f54661f6c050addb02b2d003b8
John Tenezaca <john.tenezaca7@gmail.com>
2019-04-26 16:36:34 -0400

    adding extra statements for catching error

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const NAME = 'CORNING ENDPOINT';
 
 newman.run({
     collection: 'https://www.getpostman.com/collections/07f9c25f7df2188715bb',
-    reporters: ['cli', 'htmlextra']
+    reporters: ['cli'],
+    bail: true,
+    timeoutRequest: 10000
   })
-  .on('start', 
-    function (err, args) { 
-      console.log(`Running a collection for ${ NAME }.`);
+  .on('start', function (err, args) { 
+    console.log(`Running a collection for ${ NAME }.`);
+    })
+  .on('done', function (err, summary) {
+    const executions = summary.run.executions[0];
+    const failures = summary.run.failures[0];
+    
+    const collatedErrors = [err, summary.error, executions.requestError, failures].filter(error => error);
+
+    if ( collatedErrors.length ) {
+      collatedErrors.forEach(err => {
+        console.error(`Collection run encountered ERRORs: ${ JSON.stringify(err) }.`);
+      })
     }
-  )
-  .on('done', 
-    function (err, summary) {
-      if (err || summary.error) {
-        console.error(`Collection run encountered an ERROR: ${ err || summary.error }.`);
-      }
-      else {
-        console.log(`Collection for ${ NAME } run completed.`);
-      }
+    else {
+      console.log(`Collection for ${ NAME } run completed.`);
     }
-  );
+  });
